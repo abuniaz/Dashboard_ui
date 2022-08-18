5176fabf1aa2aeafd99e4de521b2ac95963e0bf1
Abu Niaz <niaz35-507@diu.edu.bd>
2022-08-18 21:57:12 +0600

    yes

--- lib/dasboard_screen/dashboard.dart
+++ lib/dasboard_screen/dashboard.dart
@@ -1,4 +1,5 @@
 import 'package:dashboard_ui/constant.dart';
+import 'package:dashboard_ui/dasboard_screen/header.dart';
 import 'package:flutter/material.dart';
 
 class DashboardScreen extends StatelessWidget {
@@ -10,88 +11,7 @@ class DashboardScreen extends StatelessWidget {
       child: SingleChildScrollView(
         padding: const EdgeInsets.all(defaultPadding),
         child: Column(
-          children: [
-            Row(
-              children: const [
-                Text(
-                  'Dashboard',
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
-                ),
-                Spacer(),
-                Expanded(child: SearchField()),
-                ProfileCard(),
-              ],
-            )
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class ProfileCard extends StatelessWidget {
-  const ProfileCard({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.only(left: defaultPadding),
-      padding: const EdgeInsets.symmetric(
-          horizontal: defaultPadding, vertical: defaultPadding / 2),
-      decoration: BoxDecoration(
-          color: secondaryColor,
-          borderRadius: const BorderRadius.all(Radius.circular(10)),
-          border: Border.all(color: Colors.white)),
-      child: Row(children: [
-        Image.asset(
-          'images/s1.jpg',
-          height: 36,
-        ),
-        const Padding(
-          padding: EdgeInsets.symmetric(horizontal: defaultPadding / 2),
-          child: Text('Abu Niaz',
-              style: TextStyle(
-                color: Colors.white,
-              )),
-        ),
-        const Icon(
-          Icons.keyboard_arrow_down,
-          color: Colors.white,
-        )
-      ]),
-    );
-  }
-}
-
-class SearchField extends StatelessWidget {
-  const SearchField({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return TextField(
-      decoration: InputDecoration(
-        hintText: 'Search',
-        fillColor: secondaryColor,
-        filled: true,
-        border: const OutlineInputBorder(
-            borderSide: BorderSide.none,
-            borderRadius: BorderRadius.all(Radius.circular(10))),
-        suffixIcon: InkWell(
-          onTap: () {},
-          child: Container(
-            margin: const EdgeInsets.symmetric(horizontal: defaultPadding / 2),
-            decoration: const BoxDecoration(
-                color: primaryColor,
-                borderRadius: BorderRadius.all(Radius.circular(10))),
-            child: Image.asset(
-              'images/S.png',
-              height: 38,
-            ),
-          ),
+          children: const [Header()],
         ),
       ),
     );
