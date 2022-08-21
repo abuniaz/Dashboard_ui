981b5db2a24f1d3d3ccced3d0609ba7b5a1ab78f
Abu Niaz <niaz35-507@diu.edu.bd>
2022-08-21 19:12:45 +0600

    Update dashboard.dart

--- lib/dasboard_screen/dashboard.dart
+++ lib/dasboard_screen/dashboard.dart
@@ -1,5 +1,6 @@
 import 'package:dashboard_ui/constant.dart';
 import 'package:dashboard_ui/dasboard_screen/header.dart';
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 
 class DashboardScreen extends StatelessWidget {
@@ -31,17 +32,47 @@ class DashboardScreen extends StatelessWidget {
                 Expanded(
                   flex: 2,
                   child: Container(
+                    padding: const EdgeInsets.all(defaultPadding),
                     height: 500,
                     decoration: const BoxDecoration(
                         color: secondaryColor,
                         borderRadius: BorderRadius.all(Radius.circular(10))),
                     child: Column(
-                      children: const [
-                        Text(
+                      children: [
+                        const Text(
                           'Storage Details',
                           style: TextStyle(
                               fontSize: 18, fontWeight: FontWeight.w500),
-                        )
+                        ),
+                        SizedBox(
+                            height: 200,
+                            child: PieChart(PieChartData(sections: [
+                              PieChartSectionData(
+                                  value: 25,
+                                  color: primaryColor,
+                                  showTitle: false,
+                                  radius: 25),
+                              PieChartSectionData(
+                                  value: 20,
+                                  color: const Color(0xFF26E5FF),
+                                  showTitle: false,
+                                  radius: 25),
+                              PieChartSectionData(
+                                  value: 10,
+                                  color: const Color(0xFFFFCF26),
+                                  showTitle: false,
+                                  radius: 25),
+                              PieChartSectionData(
+                                  value: 25,
+                                  color: const Color(0xFFEE2727),
+                                  showTitle: false,
+                                  radius: 25),
+                              PieChartSectionData(
+                                  value: 25,
+                                  color: primaryColor.withOpacity(0.1),
+                                  showTitle: false,
+                                  radius: 25),
+                            ])))
                       ],
                     ),
                   ),
