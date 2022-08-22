1cf20e2324c44579d4ca54b4bec21116d9ee0107
Abu Niaz <niaz35-507@diu.edu.bd>
2022-08-22 18:24:12 +0600

    Update dashboard.dart

--- lib/dasboard_screen/dashboard.dart
+++ lib/dasboard_screen/dashboard.dart
@@ -1,7 +1,7 @@
 import 'package:dashboard_ui/constant.dart';
 import 'package:dashboard_ui/dasboard_screen/chart.dart';
 import 'package:dashboard_ui/dasboard_screen/header.dart';
-import 'package:fl_chart/fl_chart.dart';
+
 import 'package:flutter/material.dart';
 
 class DashboardScreen extends StatelessWidget {
@@ -39,13 +39,51 @@ class DashboardScreen extends StatelessWidget {
                         color: secondaryColor,
                         borderRadius: BorderRadius.all(Radius.circular(10))),
                     child: Column(
-                      children: const [
-                        Text(
+                      children: [
+                        const Text(
                           'Storage Details',
                           style: TextStyle(
                               fontSize: 18, fontWeight: FontWeight.w500),
                         ),
-                        Chart()
+                        const SizedBox(
+                          height: defaultPadding,
+                        ),
+                        const Chart(),
+                        Container(
+                          padding: const EdgeInsets.all(defaultPadding),
+                          decoration: BoxDecoration(
+                              border: Border.all(
+                                  width: 2,
+                                  color: primaryColor.withOpacity(0.15)),
+                              borderRadius: const BorderRadius.all(
+                                  Radius.circular(defaultPadding))),
+                          child: Row(
+                            children: [
+                              SizedBox(
+                                height: 20,
+                                width: 20,
+                                child: Image.asset('images/s1.jpg'),
+                              ),
+                              Expanded(
+                                  child: Column(
+                                children: [
+                                  const Text(
+                                    'Documents files',
+                                    maxLines: 1,
+                                    overflow: TextOverflow.ellipsis,
+                                  ),
+                                  Text(
+                                    '1329 Files',
+                                    style: Theme.of(context)
+                                        .textTheme
+                                        .caption
+                                        ?.copyWith(color: Colors.white70),
+                                  )
+                                ],
+                              ))
+                            ],
+                          ),
+                        )
                       ],
                     ),
                   ),
