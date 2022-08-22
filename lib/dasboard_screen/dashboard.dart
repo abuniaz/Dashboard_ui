583a27ee7dfb7f19df44da66243831ccbc37f59f
Abu Niaz <niaz35-507@diu.edu.bd>
2022-08-22 18:04:33 +0600

    okay

--- lib/dasboard_screen/dashboard.dart
+++ lib/dasboard_screen/dashboard.dart
@@ -1,4 +1,5 @@
 import 'package:dashboard_ui/constant.dart';
+import 'package:dashboard_ui/dasboard_screen/chart.dart';
 import 'package:dashboard_ui/dasboard_screen/header.dart';
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
@@ -38,39 +39,13 @@ class DashboardScreen extends StatelessWidget {
                         color: secondaryColor,
                         borderRadius: BorderRadius.all(Radius.circular(10))),
                     child: Column(
-                      children: [
-                        const Text(
+                      children: const [
+                        Text(
                           'Storage Details',
                           style: TextStyle(
                               fontSize: 18, fontWeight: FontWeight.w500),
                         ),
-                        SizedBox(
-                            height: 200,
-                            child: Stack(
-                              children: [
-                                PieChart(PieChartData(
-                                    sectionsSpace: 0,
-                                    centerSpaceRadius: 70,
-                                    startDegreeOffset: -90,
-                                    sections: paiChartSelectionDatas)),
-                                Positioned.fill(
-                                    child: Column(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    Text(
-                                      '29.1',
-                                      style: Theme.of(context)
-                                          .textTheme
-                                          .headline4!
-                                          .copyWith(
-                                              color: Colors.white,
-                                              fontWeight: FontWeight.w600),
-                                    ),
-                                    const Text('0f 128GB')
-                                  ],
-                                ))
-                              ],
-                            ))
+                        Chart()
                       ],
                     ),
                   ),
@@ -83,35 +58,3 @@ class DashboardScreen extends StatelessWidget {
     );
   }
 }
-
-List<PieChartSectionData> paiChartSelectionDatas = [
-  PieChartSectionData(
-    value: 25,
-    color: primaryColor,
-    showTitle: false,
-    radius: 25,
-  ),
-  PieChartSectionData(
-    value: 20,
-    color: const Color(0xFF26E5FF),
-    showTitle: false,
-    radius: 22,
-  ),
-  PieChartSectionData(
-    value: 10,
-    color: const Color(0xFFFFCF26),
-    showTitle: false,
-    radius: 19,
-  ),
-  PieChartSectionData(
-    value: 25,
-    color: const Color(0xFFEE2727),
-    showTitle: false,
-    radius: 16,
-  ),
-  PieChartSectionData(
-      value: 25,
-      color: primaryColor.withOpacity(0.1),
-      showTitle: false,
-      radius: 13),
-];
