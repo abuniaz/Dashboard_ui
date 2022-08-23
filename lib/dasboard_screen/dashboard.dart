70a77bc37433828291ea3f5a6c6e2d09ccc7a685
Abu Niaz <niaz35-507@diu.edu.bd>
2022-08-23 19:13:36 +0600

    yes

--- lib/dasboard_screen/dashboard.dart
+++ lib/dasboard_screen/dashboard.dart
@@ -1,6 +1,7 @@
 import 'package:dashboard_ui/constant.dart';
 import 'package:dashboard_ui/dasboard_screen/chart.dart';
 import 'package:dashboard_ui/dasboard_screen/header.dart';
+import 'package:dashboard_ui/dasboard_screen/storage_info.dart';
 
 import 'package:flutter/material.dart';
 
@@ -19,6 +20,7 @@ class DashboardScreen extends StatelessWidget {
               height: defaultPadding,
             ),
             Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Expanded(
                   flex: 5,
@@ -38,6 +40,7 @@ class DashboardScreen extends StatelessWidget {
                         color: secondaryColor,
                         borderRadius: BorderRadius.all(Radius.circular(10))),
                     child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: const [
                         Text(
                           'Storage Details',
@@ -80,61 +83,6 @@ class DashboardScreen extends StatelessWidget {
             )
           ],
         ),
-      ),
-    );
-  }
-}
-
-class StorageInfoCard extends StatelessWidget {
-  const StorageInfoCard({
-    Key? key,
-    required this.title,
-    required this.pic,
-    required this.amountOfFiles,
-    required this.numOfFiles,
-  }) : super(key: key);
-  final String title, pic, amountOfFiles;
-  final int numOfFiles;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.only(top: defaultPadding),
-      padding: const EdgeInsets.all(defaultPadding),
-      decoration: BoxDecoration(
-          border: Border.all(width: 2, color: primaryColor.withOpacity(0.15)),
-          borderRadius:
-              const BorderRadius.all(Radius.circular(defaultPadding))),
-      child: Row(
-        children: [
-          SizedBox(
-            height: 20,
-            width: 20,
-            child: Image.asset(pic),
-          ),
-          Expanded(
-              child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: defaultPadding),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  title,
-                  maxLines: 1,
-                  overflow: TextOverflow.ellipsis,
-                ),
-                Text(
-                  '$numOfFiles Files',
-                  style: Theme.of(context)
-                      .textTheme
-                      .caption
-                      ?.copyWith(color: Colors.white70),
-                )
-              ],
-            ),
-          )),
-          Text(amountOfFiles)
-        ],
       ),
     );
   }
