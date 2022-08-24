e9d3330ec7a22119fd84078d76f715086070ef60
Abu Niaz <niaz35-507@diu.edu.bd>
2022-08-24 10:49:56 +0600

    okay

--- lib/dasboard_screen/dashboard.dart
+++ lib/dasboard_screen/dashboard.dart
@@ -1,6 +1,7 @@
 import 'package:dashboard_ui/constant.dart';
 import 'package:dashboard_ui/dasboard_screen/chart.dart';
 import 'package:dashboard_ui/dasboard_screen/header.dart';
+import 'package:dashboard_ui/dasboard_screen/storage_details.dart';
 import 'package:dashboard_ui/dasboard_screen/storage_info.dart';
 
 import 'package:flutter/material.dart';
@@ -32,52 +33,9 @@ class DashboardScreen extends StatelessWidget {
                 const SizedBox(
                   width: defaultPadding,
                 ),
-                Expanded(
+                const Expanded(
                   flex: 2,
-                  child: Container(
-                    padding: const EdgeInsets.all(defaultPadding),
-                    decoration: const BoxDecoration(
-                        color: secondaryColor,
-                        borderRadius: BorderRadius.all(Radius.circular(10))),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: const [
-                        Text(
-                          'Storage Details',
-                          style: TextStyle(
-                              fontSize: 18, fontWeight: FontWeight.w500),
-                        ),
-                        SizedBox(
-                          height: defaultPadding,
-                        ),
-                        Chart(),
-                        StorageInfoCard(
-                          title: 'Documents files',
-                          pic: 'images/s1.jpg',
-                          amountOfFiles: '1.3GB',
-                          numOfFiles: 1328,
-                        ),
-                        StorageInfoCard(
-                          title: 'Media files',
-                          pic: 'images/s1.jpg',
-                          amountOfFiles: '15.3GB',
-                          numOfFiles: 1528,
-                        ),
-                        StorageInfoCard(
-                          title: 'Documents files',
-                          pic: 'images/s1.jpg',
-                          amountOfFiles: '1.3GB',
-                          numOfFiles: 1328,
-                        ),
-                        StorageInfoCard(
-                          title: 'Media files',
-                          pic: 'images/s1.jpg',
-                          amountOfFiles: '15.3GB',
-                          numOfFiles: 1528,
-                        ),
-                      ],
-                    ),
-                  ),
+                  child: StorageDetails(),
                 ),
               ],
             )
---
+++ lib/dasboard_screen/storage_details.dart
@@ -0,0 +1,57 @@
+import 'package:dashboard_ui/constant.dart';
+import 'package:dashboard_ui/dasboard_screen/chart.dart';
+import 'package:dashboard_ui/dasboard_screen/storage_info.dart';
+import 'package:flutter/material.dart';
+
+class StorageDetails extends StatelessWidget {
+  const StorageDetails({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(defaultPadding),
+      decoration: const BoxDecoration(
+          color: secondaryColor,
+          borderRadius: BorderRadius.all(Radius.circular(10))),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: const [
+          Text(
+            'Storage Details',
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+          ),
+          SizedBox(
+            height: defaultPadding,
+          ),
+          Chart(),
+          StorageInfoCard(
+            title: 'Documents files',
+            pic: 'images/s1.jpg',
+            amountOfFiles: '1.3GB',
+            numOfFiles: 1328,
+          ),
+          StorageInfoCard(
+            title: 'Media files',
+            pic: 'images/s1.jpg',
+            amountOfFiles: '15.3GB',
+            numOfFiles: 1528,
+          ),
+          StorageInfoCard(
+            title: 'Documents files',
+            pic: 'images/s1.jpg',
+            amountOfFiles: '1.3GB',
+            numOfFiles: 1328,
+          ),
+          StorageInfoCard(
+            title: 'Media files',
+            pic: 'images/s1.jpg',
+            amountOfFiles: '15.3GB',
+            numOfFiles: 1528,
+          ),
+        ],
+      ),
+    );
+  }
+}
